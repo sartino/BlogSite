fd59d60b416dc8ddb7da29782e5be2d562e14623
Steve Sartino <stevesartino@yahoo.com>
2015-09-30 12:52:06 -0500

    Modified 'server.js' file.

--- server/server.js
+++ server/server.js
@@ -4,30 +4,78 @@ var fs = require('fs');
 var path = require('path');
 var url = require('url');
 
-function reqResponse(res, data, contentType, statusCode) {
+function getFileExtension(route) {
+    var arr = route.split('.');
+    if(arr.length<=1) {
+        return 'html'
+    }
+    return arr[arr.length -1].toLowerCase();
+}
 
-};
+function handleRequests(req, res) {
+    var header;
+    var route = url.parse(req.url).path;
+    if(route === '/') {
+        route = '/index.html';
+    }
+    var ext = getFileExtension(route);
+    switch (ext) {
+        case 'css':
+            header = {
+                'Content-Type': 'text/css'
+            };
+            break;
+        case 'js':
+            header = {
+                'Content-Type': 'application/javascript'
+            };
+            break;
+        case 'html':
+            header = {
+                'Content-Type': 'text/html'
+            };
+            break;
+        default:
+            header = {
+                'Content-Type': 'application/json'
+            };
+            break;
+    }
+    
+    var filePath;
+        
+       filePath = path.join(__dirname, '../client' + route);
+        readFile(filePath, function(data){
+            writeResponse(res, data, header)    
+        });
+}
 
-function requestPartialHTML(req, res){
 
-};
+function writeResponse(res, data, header) {
+    var statusCode = statusCode || 200;
+    res.writeHead(statusCode, header);
+    res.end(data);
+}
 
-function requestHTML(req, res) {
+function readFile(filePath, callBack) {
+    fs.readFile(filePath, function (err, data) {
+        callBack(data);
+    });
+}
 
-};
+var server = http.createServer(handleRequests);
+var port = 3000;
 
-function requestCSS(req, res) {
+server.listen(port);
+console.log('Listening on port', port);
 
-};
+/*createServer takes in a function to handle requests.
+Here is where you can create a handler for get and post requests.
+Note: req(request) and res(response) come from node's http module.
+They include both incoming information like urls and outgoing like content */
 
-function requestJS(req, res) {
-
-};
-
-
-/*createServer takes in a function to handle requests. Here is where you can create a handler for get and post requests. Note: req(request) and res(response) come from node's http module. They include both incoming information like urls and outgoing like content */
-
-/*In order to complete the project, this callback will need to handle get requests, post requests and server up other files like css.
+/*In order to complete the project, this callback will need to handle
+get requests, post requests and serve up other files like css.
 
 Hint: creating a function to replace the anonymous function may be useful.
 		EXAMPLE:
@@ -38,35 +86,7 @@ Hint: creating a function to replace the anonymous function may be useful.
 				if(request method  === 'GET'){
 					// handle this way
 				}
-
 				...
 		};
-
-		var server = http.createServer(requestHandler)
-
-
-*/
-function requestHandler(req, res){
-	var reqURL = url.parse(req.url).pathname;
-	if(reqURL === '/'){
-		requestHTML(req, res);
-	} else if(req.url.slice(-2) === 'js'){
-		requestJS(req, res);
-	} else if(req.url.slice(-3) === 'css'){
-		requestCSS(req, res);
-	} else if(req.url.slice(-4) === 'html'){
-		requestPartialHTML(req, res);
-	} else {
-		res.statusCode = 404;
-		res.end();
-	}
-};
-
-var server = http.createServer(requestHandler);
-
-/*Requests are completed over port numbers and each one needs a unique number. Most port numbers between 0 - 1024 are reserved so here 3000 is a standard testing port to use. More on port numbers: https://en.wikipedia.org/wiki/Port_(computer_networking) */
-var port = 3000;
-
-server.listen(port);
-console.log('Listening on port', port);
+		var server = http.createServer(requestHandler)   */
 
